1fe783c6c3344901d22326255c9b0bef3cd3cc31
daanilr8 <supraaazk@gmail.com>
2024-09-30 17:58:26 +0200

    Proporcionado renderizado condicional

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React, {useState,useRef,useEffect} from "react";
+import PropTypes from "prop-types"
 
 function App() {
 
+  const usuario = {
+    nombre:"Juan",
+    apellido:"Diaz",
+    edad:"18",
+    genero:"masculino"
+  }
+
   return (
     <div>
-      <Saludo destinatario = "Mundo"></Saludo>
+      <Saludo usuario = {usuario}></Saludo>
     </div>
   );
 
@@ -14,7 +22,22 @@ function App() {
 function Saludo(props) {
   
   return (
-    <div><h1>Hola {props.destinatario}</h1></div>
-  )
+    <div>
+      {props.usuario.nombre? (<div><h1>Hola {props.usuario.nombre} {props.usuario.apellido}</h1>
+      <p>Tienes {props.usuario.edad} años</p>
+      <p>Y tu genero es {props.usuario.genero}</p></div>):
+      <h1>No se ha proporcionado el nombre</h1>}
+      </div>
+
+  );
 }
+  Saludo.propTypes = {
+
+    usuario:PropTypes.shape(
+      {
+        nombre:PropTypes.string.isRequired //El nombre del usuario es requerido, si no dara un error en consola.
+      }
+    ).isRequired
+  }
+  
 export default App;
